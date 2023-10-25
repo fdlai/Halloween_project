0ab2403d048c7d340934791d601ca17962dafd08
fdlai <112486038+fdlai@users.noreply.github.com>
2023-10-25 14:22:00 -0400

    Feature/tickets section (#8)
    * update tickets section
    
    * update: tickets section

--- pages/index.js
+++ pages/index.js
@@ -2,35 +2,88 @@
 /*                                  Elements                                  */
 /* -------------------------------------------------------------------------- */
 const addToCartButton = document.querySelector("#add-to-cart");
+const ticketPriceDropdown = document.querySelector("#ticket-type");
 const quantityDropdown = document.querySelector("#quantity");
-const cartItemsList = document.querySelector("#cart-items");
+const ticketsDescription = document.querySelector(".tickets__description");
+const cartItemsList = document.querySelector(".tickets__list");
+const ticketsTotal = document.querySelector(".tickets__total");
 const checkoutButton = document.querySelector(".tickets__checkout-button");
 const checkoutModal = document.querySelector("#modal-checkout");
+const modalTotal = document.querySelector(".modal__total");
+const ticketTemplate =
+  document.querySelector("#ticket-template").content.firstElementChild;
 /* -------------------------------------------------------------------------- */
 /*                                  Functions                                 */
 /* -------------------------------------------------------------------------- */
-function addToCart(ticketName, ticketPrice) {
+function handleCheckoutButtonState() {
+  if (cartItemsList.textContent === "") {
+    checkoutButton.disabled = true;
+  } else {
+    checkoutButton.disabled = false;
+  }
+}
+
+function addToCart() {
+  const ticketName = ticketPriceDropdown.value;
+  let ticketPrice;
+  if (ticketName === "General Admission") {
+    ticketPrice = 35;
+    ticketsDescription.textContent =
+      "General Admission access to the 13th Gate Haunted House and all attractions.";
+  } else {
+    ticketPrice = 75;
+    ticketsDescription.textContent =
+      "Immediate Access to the 13th Gate Haunted House. VIP allows you to skip Haunted House OUTSIDE line.";
+  }
+
   const selectedQuantity = quantityDropdown.value;
-  const total = ticketPrice * selectedQuantity;
+  const price = ticketPrice * selectedQuantity;
 
   // Create a new cart item element
-  const cartItem = document.createElement("li");
-  cartItem.textContent = `${ticketName} x${selectedQuantity} - Total: $${total.toFixed(
+  const cartItem = ticketTemplate.cloneNode(true);
+  const cartItemInfo = cartItem.querySelector(".ticket__info");
+  cartItemInfo.textContent = `${ticketName} x${selectedQuantity} - Price: $${price.toFixed(
     2
   )}`;
 
+  //remove ticket functionality
+  const cartItemRemoveButton = cartItem.querySelector(".ticket__remove-button");
+  cartItemRemoveButton.addEventListener("click", () => {
+    cartItem.remove();
+  });
+
   // Add the item to the cart
   cartItemsList.appendChild(cartItem);
+  const total = calculateTotal();
+  ticketsTotal.textContent = `Total: $${total.toFixed(2)}`;
+
+  handleCheckoutButtonState();
+}
+
+function calculateTotal() {
+  const nums = cartItemsList.textContent.match(/(?<=\$)\d+/g).map(parseFloat);
+  return nums.reduce((a, b) => a + b);
 }
 /* -------------------------------------------------------------------------- */
 /*                               Event Listeners                              */
 /* -------------------------------------------------------------------------- */
+ticketPriceDropdown.addEventListener("change", () => {
+  const ticketName = ticketPriceDropdown.value;
+  if (ticketName === "General Admission") {
+    ticketsDescription.textContent =
+      "General Admission access to the 13th Gate Haunted House and all attractions.";
+  } else {
+    ticketsDescription.textContent =
+      "Immediate Access to the 13th Gate Haunted House. VIP allows you to skip Haunted House OUTSIDE line.";
+  }
+});
 
 addToCartButton.addEventListener("click", () => {
   addToCart("General Admission", 35);
 });
 
 checkoutButton.addEventListener("click", () => {
+  modalTotal.textContent = `Total: $${calculateTotal().toFixed(2)}`;
   checkoutModal.classList.add("modal_opened");
 });
 
